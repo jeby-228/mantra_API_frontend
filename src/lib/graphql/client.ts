0845d8de3d1345d62cce8e25156b4dcf404cbfad
jeby-0228 <aa4188075@gmail.com>
2026-03-15 12:41:49 +0800

    fix: 修正 GraphQL 客戶端的 API URL 為正確路徑

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -1,7 +1,7 @@
 import { cacheExchange, createClient, fetchExchange } from '@urql/svelte';
 
 export const graphqlClient = createClient({
-	url: '/api/graphql',
+	url: '/graphql',
 	exchanges: [cacheExchange, fetchExchange],
 	requestPolicy: 'cache-first',
 	preferGetMethod: false
